e36958b3674060d2fc94bf3c27545f6dc94d9cb3
ashok-v7 <vashok.tkn@gmail.com>
2024-06-17 17:49:38 +0530

    uptd3-template-added

--- template.py
+++ template.py
@@ -19,10 +19,18 @@ list_of_files = [
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir, filepath = os.path.split(filepath)
-
+    filedir, filename = os.path.split(filepath)
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory {filedir} for the files {filename}")
 
-    # print(filedir)
-    # print(filepath)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file : {filepath}")
+
+    else:
+        logging.info(f"{filename} is already exists")
+
+    print(filedir)
+    print(filepath)
